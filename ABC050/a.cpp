6fa5e4a2bc7ce1c9e8b5be50aae0d6eb00b6b71a
azaberrypi <aomidori555@gmail.com>
2021-10-02 13:09:11 +0900

    [add] ABC050 A - Addition and Subtraction Easy

---
+++ ABC050/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void) {
+    int a, b;
+    char op;
+
+    std::cin >> a >> op >> b;
+
+    if (op == '+') {
+        std::cout << a + b << std::endl;
+    } else {
+        std::cout << a - b << std::endl;
+    }
+
+    return 0;
+}
