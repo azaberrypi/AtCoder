76a8df7c85184eea014845328b5357b2f2a8e24c
azaberrypi <aomidori555@gmail.com>
2021-10-03 08:32:28 +0900

    [add] ABC051 A - Haiku

---
+++ ABC051/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+    std::string s;
+    std::cin >> s;
+    s[5] = ' ';
+    s[13] = ' ';
+
+    std::cout << s << std::endl;
+
+    return 0;
+}
