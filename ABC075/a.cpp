bfa4015b25138a7ec644cae91e80fa93c23edce8
azaberrypi <aomidori555@gmail.com>
2021-10-27 09:07:25 +0900

    [add] A - One out of Three

---
+++ ABC075/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void) {
+    int a, b, c;
+    std::cin >> a >> b >> c;
+
+    if (a == b) {
+        std::cout << c << std::endl;
+    } else if (a == c) {
+        std::cout << b << std::endl;
+    } else {
+        std::cout << a << std::endl;
+    }
+
+    return 0;
+}
