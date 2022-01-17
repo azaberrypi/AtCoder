5d47a68c751a29cac9b5c4c2b2c6bbff04f038f9
azaberrypi <aomidori555@gmail.com>
2022-01-17 20:47:48 +0900

    [update] B - Sum of Three Integers

--- ABC051/b.cpp
+++ ABC051/b.cpp
@@ -5,42 +5,15 @@ int main(void) {
     std::cin >> k >> s;
 
     int counter = 0;
+    int z;
 
-    if (k * 3 < s) {
-        std::cout << 0 << std::endl;
-        return 0;
-    }
-
-    int x, y, z;
-    int y_loop_counter;
-    int even_flag;
-    for (x = k;; x--) {
-        even_flag = 0;
-        y = s - x;
-        if (y > x && y != (2 * x)) {
-            break;
-        } else if (y == 2 * x || x == 2 * y) {
-            std::cout << counter + 1 << std::endl;
-            return 0;
-        }
-        y_loop_counter = 0;
-        for (;; y--) {
+    for (int x = 0; x <= k; x++) {
+        for (int y = 0; y <= k; y++) {
             z = s - x - y;
-            if (z > y) {
-                break;
+            if (0 <= z and z <= k) {
+                counter += 1;
             }
-            if (z == y || y == x) {
-                even_flag = 1;
-            }
-            y_loop_counter++;
         }
-
-        if (even_flag == 1 && y_loop_counter != 0) {
-            counter += (y_loop_counter * 6 - 3);
-        } else if (even_flag == 0 && y_loop_counter != 0) {
-            counter += (y_loop_counter * 6);
-        }
-
     }
 
     std::cout << counter << std::endl;
